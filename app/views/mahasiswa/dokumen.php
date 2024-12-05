d44212257feb2b0fe01ebe13b8c3119c4124fede
Rensi Meila Yulvinata <54298132+rensimeila04@users.noreply.github.com>
2024-12-05 18:29:25 +0700

    fix(dokumen mahasiswa): mengubah data menjadi array, mengubah badge

--- app/views/mahasiswa/dokumen.php
+++ app/views/mahasiswa/dokumen.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" href="../../../public/assets/css/sidebar.css">
     <link rel="stylesheet" href="../../../public/assets/css/header.css">
     <link rel="stylesheet" href="../../../public/assets/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dokumen - SIBETA</title>
@@ -37,14 +38,11 @@
                             <div class="d-flex justify-content-between mb-3">
                                 <div class="input-group w-25" style="border-radius: 8px;">
                                     <span class="input-group-text" id="basic-addon1" style="background-color: #FFFFFF;">
-                                        <span class="material-symbols-outlined">
-                                            search
-                                        </span>
+                                        <i class="bi bi-search" style="color: #ADB5BD; font-size: 16px;"></i>
                                     </span>
                                     <input type="text" class="form-control" placeholder="Cari dokumen..." aria-label="Sarch" aria-describedby="basic-addon1" style="border-left: none;">
                                     <button class="btn" style="margin-left: 10px; color:#fff; background-color: #3E368C; border-radius: 4px; height: 35px;">Cari</button>
                                 </div>
-
                             </div>
 
                             <table class="table table-striped">
@@ -59,120 +57,64 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td class=" text-truncate" style="max-width: 50px;">Tanda Terima Penyerahan Laporan Tugas Akhir/Skripsi ke Ruang Baca</td>
-                                        <td scope="row">Administratif</td>
-                                        <td>12 November 2024</td>
-                                        <td>
-                                            <span class="badge bg-success" style="border-radius: 16px; font-size: 16px; height: 35px; font-weight: 400; padding-top: 7px">Terverifikasi</span>
-                                        </td>
-                                        <td><!-- Button trigger modal -->
-                                            <div class="aksi">
-                                                <a href="detail_dokumen.php" style="text-decoration: none;">
-                                                    <button type="button" class="btn" style="color:#FFFFFF; background-color: #3E368C;">
-                                                        Detail
-                                                    </button>
-                                                </a>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                    <th scope="row">2</th>
-                                        <td class=" text-truncate" style="max-width: 50px;">Tanda Terima Penyerahan Laporan PKL/Magang ke Ruang Baca</td>
-                                        <td scope="row">Administratif</td>
-                                        <td>12 November 2024</td>
-                                        <td><span class="badge bg-warning" style="border-radius: 16px; font-size: 16px; height: 35px; font-weight: 400; padding-top: 7px; width: 105px;">Diajukan</span></td>
-                                        <td><!-- Button trigger modal -->
-                                            <div class="aksi">
-                                                <a href="detail_dokumen.php" style="text-decoration: none;">
-                                                    <button type="button" class="btn" style="color:#FFFFFF; background-color: #3E368C;">
-                                                        Detail
-                                                    </button>
-                                                </a>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                    <th scope="row">3</th>
-                                        <td class=" text-truncate" style="max-width: 50px;">Surat Bebas Kompen</td>
-                                        <td scope="row">Administratif</td>
-                                        <td>12 November 2024</td>
-                                        <td><span class="badge bg-danger" style="border-radius: 16px; font-size: 16px; height: 35px; font-weight: 400; padding-top: 7px; width: 105px;">Ditolak</span></td>
-                                        <td><!-- Button trigger modal -->
-                                            <div class="aksi">
-                                                <a href="detail_dokumen.php" style="text-decoration: none;">
-                                                    <button type="button" class="btn" style="color:#FFFFFF; background-color: #3E368C;">
-                                                        Detail
-                                                    </button>
-                                                </a>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                    <th scope="row">4</th>
-                                        <td class=" text-truncate" style="max-width: 50px;">Scan TOEIC</td>
-                                        <td scope="row">Administratif</td>
-                                        <td>12 November 2024</td>
-                                        <td><span class="badge bg-warning" style="border-radius: 16px; font-size: 16px; height: 35px; font-weight: 400; padding-top: 7px; width: 105px;">Diajukan</span></td>
-                                        <td><!-- Button trigger modal -->
-                                            <div class="aksi">
-                                                <a href="detail_dokumen.php"  style="text-decoration: none;">
-                                                    <button type="button" class="btn" style="color:#FFFFFF; background-color: #3E368C;">
-                                                        Detail
-                                                    </button>
-                                                </a>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                    <th scope="row">5</th>
-                                        <td class=" text-truncate" style="max-width: 50px;">Laporan Tugas Akhir/Skripsi</td>
-                                        <td scope="row">Teknis</td>
-                                        <td>12 November 2024</td>
-                                        <td><span class="badge bg-warning" style="border-radius: 16px; font-size: 16px; height: 35px; font-weight: 400; padding-top: 7px; width: 105px;">Diajukan</span></td>
-                                        <td><!-- Button trigger modal -->
-                                            <div class="aksi">
-                                                <a href="detail_dokumen.php"  style="text-decoration: none;">
-                                                    <button type="button" class="btn" style="color:#FFFFFF; background-color: #3E368C;">
-                                                        Detail
-                                                    </button>
-                                                </a>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                    <th scope="row">6</th>
-                                        <td class=" text-truncate" style="max-width: 50px;">Buku Panduan Aplikasi</td>
-                                        <td scope="row">Teknis</td>
-                                        <td>12 November 2024</td>
-                                        <td><span class="badge bg-warning" style="border-radius: 16px; font-size: 16px; height: 35px; font-weight: 400; padding-top: 7px; width: 105px;">Diajukan</span></td>
-                                        <td><!-- Button trigger modal -->
-                                            <div class="aksi">
-                                                <a href="detail_dokumen.php"  style="text-decoration: none;">
-                                                    <button type="button" class="btn" style="color:#FFFFFF; background-color: #3E368C;">
-                                                        Detail
-                                                    </button>
-                                                </a>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                    <th scope="row">7</th>
-                                        <td class=" text-truncate" style="max-width: 50px;">Surat Pernyataan Publikasi</td>
-                                        <td scope="row">Teknis</td>
-                                        <td>12 November 2024</td>
-                                        <td><span class="badge bg-warning" style="border-radius: 16px; font-size: 16px; height: 35px; font-weight: 400; padding-top: 7px; width: 105px;">Diajukan</span></td>
-                                        <td><!-- Button trigger modal -->
-                                            <div class="aksi">
-                                                <a href="detail_dokumen.php"  style="text-decoration: none;">
-                                                    <button type="button" class="btn" style="color:#FFFFFF; background-color: #3E368C;">
-                                                        Detail
-                                                    </button>
-                                                </a>
-                                            </div>
-                                        </td>
-                                    </tr>
+                                    <?php
+                                    $documents = [
+                                        [
+                                            'name' => 'Tanda Terima Penyerahan Laporan Tugas Akhir/Skripsi ke Ruang Baca',
+                                            'type' => 'Administratif',
+                                            'date' => '12 November 2024',
+                                            'status' => 'Ditolak'
+                                        ],
+                                        [
+                                            'name' => 'Tanda Terima Penyerahan Laporan PKL/Magang ke Ruang Baca',
+                                            'type' => 'Administratif',
+                                            'date' => '12 November 2024',
+                                            'status' => 'Diajukan'
+                                        ],
+                                        [
+                                            'name' => 'Surat Bebas Kompen',
+                                            'type' => 'Administratif',
+                                            'date' => '12 November 2024',
+                                            'status' => 'Ditolak'
+                                        ],
+                                    ];
+                                    foreach ($documents as $index => $document) {
+                                        $badgeClass = '';
+                                        switch ($document['status']) {
+                                            case 'Terverifikasi':
+                                                $badgeClass = 'bg-success';
+                                                break;
+                                            case 'Diajukan':
+                                                $badgeClass = 'bg-warning';
+                                                break;
+                                            case 'Ditolak':
+                                                $badgeClass = 'bg-danger';
+                                                break;
+                                        }
+                                    ?>
+                                        <tr>
+                                            <th scope="row"><?php echo $index + 1; ?></th>
+                                            <td class="text-truncate" style="max-width: 50px;"><?php echo $document['name']; ?></td>
+                                            <td scope="row"><?php echo $document['type']; ?></td>
+                                            <td><?php echo $document['date']; ?></td>
+                                            <td>
+                                                <span class="badge <?php echo $badgeClass; ?>" style="border-radius: 16px; font-size: 16px; height: 35px; width: 126px; font-weight: 400; padding-top: 7px;">
+                                                    <?php echo $document['status']; ?>
+                                                </span>
+                                            </td>
+                                            <td>
+                                                <div class="aksi">
+                                                    <a href="detail_dokumen.php" style="text-decoration: none;">
+                                                        <button type="button" class="btn" style="color:#FFFFFF; background-color: #3E368C;">
+                                                            Detail
+                                                        </button>
+                                                    </a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    <?php
+                                    }
+                                    ?>
                                 </tbody>
                             </table>
                         </div>
@@ -180,9 +122,6 @@
                 </div>
             </div>
         </div>
-
-
-
     </div>
     <!-- <script src="components/sidebar/script.js"></script> -->
 </body>
