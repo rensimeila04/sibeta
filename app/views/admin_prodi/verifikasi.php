596f93fedfe24ed56273753a825e2b0865d2be5c
Rensi Meila Yulvinata <54298132+rensimeila04@users.noreply.github.com>
2024-12-05 11:07:12 +0700

    init(verifikasi admin): menambahkan header sidebar breadcrumps

---
+++ app/views/admin_prodi/verifikasi.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="../components/sidebar_admin.html">
+    <link rel="stylesheet" href="../components/header_admin.html">
+    <link rel="stylesheet" href="../../../public/assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profil</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <?php
+        include '../components/sidebar_admin.html';
+        ?>
+        <div class="main">
+            <?php
+            include '../components/header_admin.html';
+            ?>
+            <div class="p-4 dashboard">
+                <div class="breadcrumbs mb-3">
+                    <span class="material-symbols-outlined">home</span>
+                    <a href="#">SIBETA</a>
+                    <span class="separator">/</span>
+                    <a href="#">Kelola Dokumen</a>
+                    <span class="separator">/</span>
+                    <a href="#">Detal Mahasiswa</a>
+                    <span class="separator">/</span>
+                    <span>Detail Dokumen</span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
