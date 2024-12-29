c92f90b5ce54c363071c1542add7401f271eaa7e
Rensi Meila Yulvinata <54298132+rensimeila04@users.noreply.github.com>
2024-12-29 14:10:37 +0700

    feat(notif): fe notif

--- app/views/components/header_mahasiswa.php
+++ app/views/components/header_mahasiswa.php
@@ -18,45 +18,77 @@
 
 <body>
     <div class="header">
-        <div class="notification">
-            <span class="material-symbols-outlined ">
-                notifications
-            </span>
-        </div>
-
-        <div class="profile">
-            <div class="profile-info">
-                <div class="profile-name">
-                    <?= $nama ?>
-                </div>
-                <div class="profile-nim">
-                    <?= $nim ?>
-                </div>
-            </div>
-
-            <div class="profile-settings d-flex flex-row align-items-center">
-                <img src="<?php echo '../app/' . $photo_profile_path; ?>" alt="avatar" style="width: 40px; height: 40px;">
-                <div class="dropdown">
-                    <button
-                        class="border-0 bg-transparent d-flex align-items-center"
+        <div class="header-left">
+            <div class="notification">
+                <div class="dropdown dropdown-menu-notif">
+                    <button class="border-0 bg-transparent d-flex align-items-center position-relative"
                         data-bs-toggle="dropdown"
                         aria-expanded="false">
-                        <span class="material-symbols-outlined">keyboard_arrow_down</span>
+                        <span class="material-symbols-outlined">
+                            notifications
+                        </span>
+                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                            2
+                        </span>
                     </button>
                     <ul class="dropdown-menu">
-                        <li>
-                            <div class="d-flex flex-row align-items-center">
-                                <span class="material-symbols-outlined" style="margin-left: 15px;">
-                                    person
-                                </span>
-                                <a href="/sibeta/public/index.php?page=profil_mahasiswa"
-                                    class="dropdown-item border-0 bg-transparent"
-                                    style="color: #212529;">
-                                    Profil Mahasiswa
-                                </a>
-                            </div>
+                        <li class="notification-header">
+                            <h6 class="m-0 p-2 text-center border-bottom">Notifikasi</h6>
+                        </li>
+                        <li class="notification-item">
+                            <a href="#" class="dropdown-item notification-link">
+                                <div class="notification-content">
+                                    <p class="notification-text mb-0">Dokumen anda telah di verifikasi</p>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="notification-item">
+                            <a href="#" class="dropdown-item notification-link">
+                                <div class="notification-content">
+                                    <p class="notification-text mb-0">Dokumen anda di tolak</p>
+                                </div>
+                            </a>
                         </li>
                     </ul>
+                </div>
+            </div>
+        </div>
+
+        <!-- Profile section moved to right -->
+        <div class="header-right">
+            <div class="profile">
+                <div class="profile-info">
+                    <div class="profile-name">
+                        <?= $nama ?>
+                    </div>
+                    <div class="profile-nim">
+                        <?= $nim ?>
+                    </div>
+                </div>
+
+                <div class="profile-settings d-flex flex-row align-items-center">
+                    <img src="<?php echo '../app/' . $photo_profile_path; ?>" alt="avatar" style="width: 40px; height: 40px;">
+                    <div class="dropdown">
+                        <button class="border-0 bg-transparent d-flex align-items-center"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            <span class="material-symbols-outlined">keyboard_arrow_down</span>
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-profil">
+                            <li>
+                                <div class="d-flex flex-row align-items-center">
+                                    <span class="material-symbols-outlined" style="margin-left: 15px;">
+                                        person
+                                    </span>
+                                    <a href="/sibeta/public/index.php?page=profil_mahasiswa"
+                                        class="dropdown-item border-0 bg-transparent"
+                                        style="color: #212529;">
+                                        Profil Mahasiswa
+                                    </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
